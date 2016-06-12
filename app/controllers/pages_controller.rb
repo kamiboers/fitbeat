07913acfed86276aef13549e02c9068f6c668819
Kami Boers <kamiboers@gmail.com>
2016-06-12 13:56:55 -0600

    styling continued

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,10 @@ end
 
 def dashboard
   @user = current_user
+  flash[:success] = "Welcome, #{@user.name} You've successfully logged in."
   yesterday = (DateTime.now - 1.day).strftime("%Y-%m-%d")
   response = HTTParty.get "https://api.fitbit.com/1/user/#{@user.fitbit_credential.uid}/activities/heart/date/today/1w.json", headers: {"Authorization" => "Bearer #{@user.fitbit_credential.token}"}
   @heart_data = HeartData.new(response) if response
-  # binding.pry
   response = HTTParty.get "https://api.spotify.com/v1/me/playlists", headers: {"Authorization" => "Bearer #{@user.spotify_credential.token}"}
   @spotify_data = response
 end
