ddd2baab9332b94015cb5febd2f9e8c147c70dc7
Kami Boers <kamiboers@gmail.com>
2016-06-12 22:03:39 -0600

    refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,3 @@
-require 'base64'
 class PagesController < ApplicationController
 
 def index
@@ -7,8 +6,7 @@ end
 
 def dashboard
   @user = current_user
-  flash[:success] = "Welcome, #{@user.name} You've successfully logged in."
-  yesterday = (DateTime.now - 1.day).strftime("%Y-%m-%d")
+  # flash[:success] = "Welcome, #{@user.name} You've successfully logged in."
   response = HTTParty.get "https://api.fitbit.com/1/user/#{@user.fitbit_credential.uid}/activities/heart/date/today/1w.json", headers: {"Authorization" => "Bearer #{@user.fitbit_credential.token}"}
   @heart_data = HeartData.new(response) if response
   response = HTTParty.get "https://api.spotify.com/v1/me/playlists", headers: {"Authorization" => "Bearer #{@user.spotify_credential.token}"}
